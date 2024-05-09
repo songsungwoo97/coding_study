3bfe8353b79c4d500b8508c3bfa0e7a7d1e68c8b
Song Sung Woo <59758042+songsungwoo97@users.noreply.github.com>
2024-05-09 12:19:29 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+string gear[4];
+int isRotate[4];
+
+void move(int idx, int clock)
+{
+  if (clock == 1)
+    gear[idx] = gear[idx].substr(7) + gear[idx].substr(0, 7);
+  else if (clock == -1)
+    gear[idx] = gear[idx].substr(1, 7) + gear[idx].substr(0, 1);
+}
+
+void left_check(int idx, int clock)
+{
+  if (idx <= 0)
+    return; // 첫 톱니바퀴
+  if (gear[idx][6] != gear[idx - 1][2])
+  { // 같으면 움직임x
+    isRotate[idx - 1] = clock * -1;
+    left_check(idx - 1, clock * -1);
+  }
+}
+
+void right_check(int idx, int clock)
+{
+  if (idx >= 3)
+    return; // 마지막 톱니바퀴
+  if (gear[idx][2] != gear[idx + 1][6])
+  { // 같으면 움직임x
+    isRotate[idx + 1] = clock * -1;
+    right_check(idx + 1, clock * -1);
+  }
+}
+
+void check(int idx, int clock)
+{
+  isRotate[idx] = clock;
+  left_check(idx, clock);
+  right_check(idx, clock);
+
+  for (int i = 0; i < 4; i++)
+    move(i, isRotate[i]);
+}
+
+int main()
+{
+  int ans = 0;
+  for (int i = 0; i < 4; i++)
+  {
+    cin >> gear[i];
+  }
+  int k;
+  int g;
+  int r;
+  cin >> k;
+  for (int i = 0; i < k; i++)
+  {
+    cin >> g >> r;
+
+    for (int i = 0; i < 4; i++)
+      isRotate[i] = 0;
+
+    check(g - 1, r);
+  }
+  int mul = 1;
+  for (int i = 0; i < 4; i++)
+  {
+    ans += (gear[i][0] - '0') * mul;
+    mul *= 2;
+  }
+
+  cout << ans << '\n';
+  return 0;
+}
